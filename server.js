cceb06b9fc58bb24291cc3b11a2cab23794e5d5d
rogermiler256 <rogerguzmanventas@gmail.com>
2025-10-27 04:14:12 -0500

    B

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+app.use(express.static(__dirname)); // Servir archivos estáticos
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
